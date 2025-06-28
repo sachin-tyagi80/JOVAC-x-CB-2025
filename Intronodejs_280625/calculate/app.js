9aae81297d7589c76168f8bee5921fff8479afa3
Sachin Tyagi <sachintyagi6322@gmail.com>
2025-06-28 20:45:15 +0530

    Create app.js

---
+++ Intronodejs_280625/calculate/app.js
@@ -0,0 +1,13 @@
+// var a = "GLA STUDENTS";
+
+// console.log(a);
+
+// var aa = 20;
+// var bb = 30;
+
+// console.log(aa+bb);
+
+// console.log(global);
+
+console.log(this);
+// console.log(window);
